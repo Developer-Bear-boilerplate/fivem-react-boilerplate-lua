87703a3f8c6ac470817d7c48915f0430706231ae
Developer-Bear <collindudra@gmail.com>
2023-05-13 19:36:20 -0500

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,10 @@
 fx_version "cerulean"
 
-description "Basic React (TypeScript) & Lua Game Scripts Boilerplate"
-author "Project Error"
+description ""
+author "Bear#6792"
 version '1.0.0'
-repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
-
 lua54 'yes'
-
-games {
-  "gta5",
-  "rdr3"
-}
+game "gta5"
 
 ui_page 'web/build/index.html'
 
